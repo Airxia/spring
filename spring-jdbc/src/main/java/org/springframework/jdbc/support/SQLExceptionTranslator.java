077dd643583c7d6adba3950ded518337289ffa3e
alvin <491783100@qq.com>
2019-05-04 22:42:30 +0800

    Initial commit

---
+++ spring-jdbc/src/main/java/org/springframework/jdbc/support/SQLExceptionTranslator.java
@@ -0,0 +1,60 @@
+/*
+ * copyright 2002-2017 the original author or authors.
+ *
+ * licensed under the apache license, version 2.0 (the "license");
+ * you may not use this file except in compliance with the license.
+ * you may obtain a copy of the license at
+ *
+ *      http://www.apache.org/licenses/license-2.0
+ *
+ * unless required by applicable law or agreed to in writing, software
+ * distributed under the license is distributed on an "as is" basis,
+ * without warranties or conditions of any kind, either express or implied.
+ * see the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package org.springframework.jdbc.support;
+
+import java.sql.SQLException;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.lang.Nullable;
+
+/**
+ * Strategy interface for translating between {@link SQLException SQLExceptions}
+ * and Spring's data access strategy-agnostic {@link DataAccessException}
+ * hierarchy.
+ *
+ * <p>Implementations can be generic (for example, using
+ * {@link java.sql.SQLException#getSQLState() SQLState} codes for JDBC) or wholly
+ * proprietary (for example, using Oracle error codes) for greater precision.
+ *
+ * @author Rod Johnson
+ * @author Juergen Hoeller
+ * @see org.springframework.dao.DataAccessException
+ */
+@FunctionalInterface
+public interface SQLExceptionTranslator {
+
+	/**
+	 * Translate the given {@link SQLException} into a generic {@link DataAccessException}.
+	 * <p>The returned DataAccessException is supposed to contain the original
+	 * {@code SQLException} as root cause. However, client code may not generally
+	 * rely on this due to DataAccessExceptions possibly being caused by other resource
+	 * APIs as well. That said, a {@code getRootCause() instanceof SQLException}
+	 * check (and subsequent cast) is considered reliable when expecting JDBC-based
+	 * access to have happened.
+	 * @param task readable text describing the task being attempted
+	 * @param sql SQL query or update that caused the problem (if known)
+	 * @param ex the offending {@code SQLException}
+	 * @return the DataAccessException wrapping the {@code SQLException},
+	 * or {@code null} if no translation could be applied
+	 * (in a custom translator; the default translators always throw an
+	 * {@link org.springframework.jdbc.UncategorizedSQLException} in such a case)
+	 * @see org.springframework.dao.DataAccessException#getRootCause()
+	 */
+	@Nullable
+	DataAccessException translate(String task, @Nullable String sql, SQLException ex);
+
+}
